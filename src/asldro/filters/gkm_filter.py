3190362bb7c79e60a56b8ee2f7ccdb278eab53b7
Aaron Oliver-Taylor <aaron.oliver-taylor@goldstandardphantoms.com>
2020-08-26 17:30:45 +0100

    ASLDRO-5: updated input validation to include data ranges and test for this

--- src/asldro/filters/gkm_filter.py
+++ src/asldro/filters/gkm_filter.py
@@ -14,6 +14,10 @@ KEY_LABEL_EFFICIENCY = "label_efficiency"
 KEY_LAMBDA_BLOOD_BRAIN = "lambda_blood_brain"
 KEY_T1_ARTERIAL_BLOOD = "t1_arterial_blood"
 
+CASL = "CASL"
+PCASL = "pCASL"
+PASL = "PASL"
+
 KEYS_TUPLE = (
     KEY_PERFUSION_RATE,
     KEY_TRANSIT_TIME,
@@ -26,31 +30,15 @@ KEYS_TUPLE = (
     KEY_T1_ARTERIAL_BLOOD,
 )
 
-KEYS_DTYPE_DICT = {
-    KEY_PERFUSION_RATE: BaseImageContainer,
-    KEY_TRANSIT_TIME: BaseImageContainer,
-    KEY_M0: BaseImageContainer,
-    KEY_LABEL_TYPE: str,
-    KEY_LABEL_DURATION: float,
-    KEY_SIGNAL_TIME: float,
-    KEY_LABEL_EFFICIENCY: float,
-    KEY_LAMBDA_BLOOD_BRAIN: float,
-    KEY_T1_ARTERIAL_BLOOD: float,
-}
-
-CASL = "CASL"
-PCASL = "pCASL"
-PASL = "PASL"
-
 
 class GkmFilter(BaseFilter):
-    """ 
+    """
     A filter that generates the ASL signal using the General Kinetic Model
-  
+
     Inputs:
 
     Outputs:
-   
+
     """
 
     def __init__(self):
@@ -77,13 +65,62 @@ class GkmFilter(BaseFilter):
         'lambda_blood_brain' must be a float between 0 and 1
         't1_arterial_blood' must be a float between 0 and 100
          """
+
+        # Dictionary for data validation.  Each entry has key corresponding to
+        # the inputs keyword for the filter, then the value is a tuple with:
+        # [0] - the data type
+        # [1:] - the data range.
+        #       - for numeric types this is the min and max (inclusive)
+        #       - for BaseImageContainer this is the min and max of the .image (inclusive)
+        #       = for strings this is a one or more strings to match against
+        inputs_datavalid_dict = {
+            KEY_PERFUSION_RATE: (BaseImageContainer, 0, float("Inf")),
+            KEY_TRANSIT_TIME: (BaseImageContainer, 0, float("Inf")),
+            KEY_M0: (BaseImageContainer, 0, float("Inf")),
+            KEY_LABEL_TYPE: (str, CASL, PCASL, PASL),
+            KEY_LABEL_DURATION: (float, 0, 100),
+            KEY_SIGNAL_TIME: (float, 0, 100),
+            KEY_LABEL_EFFICIENCY: (float, 0, 1),
+            KEY_LAMBDA_BLOOD_BRAIN: (float, 0, 1),
+            KEY_T1_ARTERIAL_BLOOD: (float, 0, 100),
+        }
         # Loop over the keys
         for key in KEYS_TUPLE:
             # Check if key is present
             if key not in self.inputs:
                 raise FilterInputValidationError(f"{self} does not have defined {key}")
 
-            if not isinstance(self.inputs[key], KEYS_DTYPE_DICT[key]):
+            # Key present, check data type
+            input_value = self.inputs[key]
+            if not isinstance(input_value, inputs_datavalid_dict[key][0]):
                 raise FilterInputValidationError(
-                    f"{self} is not a {KEYS_DTYPE_DICT[key]} (is {type(self.inputs[key])})"
+                    f"{self} is not a {inputs_datavalid_dict[key][0]} (is {type(input_value)})"
                 )
+
+            # Data type OK, check value is within limits
+            # float or int
+            if inputs_datavalid_dict[key][0] in (int, float):
+                min_val = inputs_datavalid_dict[key][1]
+                max_val = inputs_datavalid_dict[key][2]
+                if input_value < min_val or input_value > max_val:
+                    raise FilterInputValidationError(
+                        f"{self} is not between {min_val} and {max_val} (is {input_value}"
+                    )
+            # BaseImageContainer derived
+            if inputs_datavalid_dict[key][0] == BaseImageContainer:
+                min_val = inputs_datavalid_dict[key][1]
+                max_val = inputs_datavalid_dict[key][2]
+                if (input_value.image < min_val).any() or (
+                    input_value.image > max_val
+                ).any():
+                    raise FilterInputValidationError(
+                        f"{self} has values outside of the range {min_val} to {max_val}"
+                    )
+
+            # string
+            if inputs_datavalid_dict[key][0] == str:
+                match_strings = inputs_datavalid_dict[key][1:]
+                if input_value not in match_strings:
+                    raise FilterInputValidationError(
+                        f"{self} is not between {min_val} and {max_val} (is {input_value}"
+                    )
--- src/asldro/filters/test_gkm_filter.py
+++ src/asldro/filters/test_gkm_filter.py
@@ -6,36 +6,28 @@ import numpy as np
 import numpy.testing
 from asldro.containers.image import BaseImageContainer, NumpyImageContainer
 from asldro.filters.basefilter import BaseFilter, FilterInputValidationError
-from asldro.filters.gkm_filter import (
-    GkmFilter,
-    KEY_PERFUSION_RATE,
-    KEY_TRANSIT_TIME,
-    KEY_M0,
-    KEY_LABEL_TYPE,
-    KEY_LABEL_DURATION,
-    KEY_SIGNAL_TIME,
-    KEY_LABEL_EFFICIENCY,
-    KEY_LAMBDA_BLOOD_BRAIN,
-    KEY_T1_ARTERIAL_BLOOD,
-    KEYS_TUPLE,
-    CASL,
-    PCASL,
-    PASL,
-)
+from asldro.filters.gkm_filter import GkmFilter
 
 TEST_VOLUME_DIMENSIONS = (32, 32, 32)
+TEST_IMAGE_ONES = NumpyImageContainer(image=np.ones(TEST_VOLUME_DIMENSIONS))
+TEST_IMAGE_NEG = NumpyImageContainer(image=-1.0 * np.ones(TEST_VOLUME_DIMENSIONS))
+TEST_IMAGE_101 = NumpyImageContainer(image=100.0 + np.ones(TEST_VOLUME_DIMENSIONS))
 
+# test data dictionary, [0] in each tuple passes, after that should fail validation
 TEST_DATA_DICT = {
-    KEY_PERFUSION_RATE: NumpyImageContainer(image=np.zeros(TEST_VOLUME_DIMENSIONS)),
-    KEY_TRANSIT_TIME: NumpyImageContainer(image=np.zeros(TEST_VOLUME_DIMENSIONS)),
-    KEY_M0: NumpyImageContainer(image=np.zeros(TEST_VOLUME_DIMENSIONS)),
-    KEY_LABEL_TYPE: "pCASL",
-    KEY_LABEL_DURATION: 1.8,
-    KEY_SIGNAL_TIME: 3.6,
-    KEY_LABEL_EFFICIENCY: 0.85,
-    KEY_LAMBDA_BLOOD_BRAIN: 0.9,
-    KEY_T1_ARTERIAL_BLOOD: 1.6,
+    "perfusion_rate": (TEST_IMAGE_ONES, TEST_IMAGE_NEG),
+    "transit_time": (TEST_IMAGE_ONES, TEST_IMAGE_NEG),
+    "m0": (TEST_IMAGE_ONES, TEST_IMAGE_NEG),
+    "label_type": ("pCASL", "casl", "PSL", "str"),
+    "label_duration": (1.8, -0.1, 101.0),
+    "signal_time": (3.6, -0.1, 101.0),
+    "label_efficiency": (0.85, -0.1, 1.01),
+    "lambda_blood_brain": (0.9, -0.1, 1.01),
+    "t1_arterial_blood": (1.6, -0.1, 101.0),
 }
+CASL = "CASL"
+PCASL = "PCASL"
+PASL = "PASL"
 
 
 def test_gkm_filter_validate_inputs():
@@ -43,21 +35,38 @@ def test_gkm_filter_validate_inputs():
     inputs to the GkmFilter are incorrect or missing
     """
 
-    for inputs_key in KEYS_TUPLE:
+    for inputs_key in TEST_DATA_DICT:
         gkm_filter = GkmFilter()
         test_data = deepcopy(TEST_DATA_DICT)
         # remove the corresponding key from test_data
         test_data.pop(inputs_key)
 
         for data_key in test_data:
-            gkm_filter.add_input(data_key, test_data[data_key])
+            gkm_filter.add_input(data_key, test_data[data_key][0])
 
         # Key not defined
         with pytest.raises(FilterInputValidationError):
             gkm_filter.run()
 
-        # Key has wrong data
+        # Key has wrong data type
         gkm_filter.add_input(inputs_key, None)
         with pytest.raises(FilterInputValidationError):
             gkm_filter.run()
 
+        # Data not in the valid range
+        for test_value in TEST_DATA_DICT[inputs_key][1:]:
+            # re-initialise filter
+            gkm_filter = GkmFilter()
+
+            # add valid inputs
+            for data_key in test_data:
+                gkm_filter.add_input(data_key, test_data[data_key][0])
+
+            # add invalid input and check a FilterInputValidationError is raised
+            gkm_filter.add_input(inputs_key, test_value)
+            with pytest.raises(FilterInputValidationError):
+                gkm_filter.run()
+
+
+if __name__ == "__main__":
+    test_gkm_filter_validate_inputs()
