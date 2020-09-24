67ad6582990e8a6435b4b8124b4cd7053046096e
Aaron Oliver-Taylor <aaron.oliver-taylor@golstandardphantoms.com>
2020-09-24 15:53:04 +0100

    ASLDRO-84: Created AffineMatrixFilter class

---
+++ src/asldro/filters/affine_matrix_filter.py
@@ -0,0 +1,219 @@
+""" Affine Matrix Filter """
+
+import numpy as np
+from asldro.filters.basefilter import BaseFilter, FilterInputValidationError
+from asldro.validators.parameters import (
+    ParameterValidator,
+    Parameter,
+    greater_than_equal_to_validator,
+    from_list_validator,
+    isinstance_validator,
+    list_of_type_validator,
+    for_each_validator,
+    range_inclusive_validator,
+)
+
+
+class AffineMatrixFilter(BaseFilter):
+    """A filter that creates an affine transformation matrix
+    based on input parameters for rotation, translation, and scaling
+    Conventions are for RAS+ coordinate systems only
+
+    Order of transformations is (first to last):
+    translate to rotation origin
+    rotate about x
+    rotate about y
+    rotate about z
+    translate back to original location
+    translate
+    scale
+
+    ### Inputs
+    Parameters are all keyword arguments for the add_inputs() member function
+
+    :param rotation: angles to rotate about the x, y and z axes in degrees
+    (-180 to 180 degrees inclusive, default (0, 0, 0))
+    :type rotation: Tuple[float, float, float]
+    :param rotation_origin: coordinates of the point to perform rotations about (default (0, 0, 0))
+    :type rotation_origin: Tuple[float, float, float]
+    :param translation: amount to translate along the x, y and z axes (default (0, 0, 0))
+    :type translation: Tuple[float, float, float]
+    :param scale: scaling factors along each axis (default (1, 1, 1))
+    :type scale: Tuple[float, float, float]
+    :param affine: input 4x4 affine matrix to apply transformation to (default `numpy.eye(4)`)
+    :type affine: np.ndarray(4)
+
+    ### Outputs
+    :param affine: output 4x4 affine matrix with all transformations combined.
+    All input parameters are optional, if no inputs are supplied
+    `'affine' = numpy.eye(4)`
+    """
+
+    KEY_ROTATION = "rotation"
+    KEY_ROTATION_ORIGIN = "rotation_origin"
+    KEY_TRANSLATION = "translation"
+    KEY_SCALE = "scale"
+    KEY_AFFINE = "affine"
+
+    def __init(self):
+        super().__init__(name="Compute Affine Matrix")
+
+    def _run(self):
+        # construct individual transformation matrices
+        input_affine: np.ndarray = self.inputs[AffineMatrixFilter.KEY_AFFINE]
+        rotation_angles: tuple(float) = np.radians(
+            self.inputs[AffineMatrixFilter.KEY_ROTATION]
+        )
+        rotation_origin: tuple(float) = self.inputs[
+            AffineMatrixFilter.KEY_ROTATION_ORIGIN
+        ]
+        translation: tuple(float) = self.inputs[AffineMatrixFilter.KEY_TRANSLATION]
+        scale: tuple(float) = self.inputs[AffineMatrixFilter.KEY_SCALE]
+
+        scale_matrix = np.array(
+            (
+                (scale[0], 0, 0, 0),
+                (0, scale[1], 0, 0),
+                (0, 0, scale[2], 0),
+                (0, 0, 0, 1),
+            )
+        )
+        translation_matrix = np.array(
+            (
+                (1, 0, 0, translation[0]),
+                (0, 1, 0, translation[1]),
+                (0, 0, 1, translation[2]),
+                (0, 0, 0, 1),
+            )
+        )
+        rotation_origin_translation_matrix = np.array(
+            (
+                (1, 0, 0, -rotation_origin[0]),
+                (0, 1, 0, -rotation_origin[1]),
+                (0, 0, 1, -rotation_origin[2]),
+                (0, 0, 0, 1),
+            )
+        )
+
+        rotation_x_matrix = np.array(
+            (
+                (1, 0, 0, 0),
+                (0, np.cos(rotation_angles[0]), -np.sin(rotation_angles[0]), 0),
+                (0, np.sin(rotation_angles[0]), np.cos(rotation_angles[0]), 0),
+                (0, 0, 0, 1),
+            )
+        )
+        rotation_y_matrix = np.array(
+            (
+                (np.cos(rotation_angles[1]), 0, np.sin(rotation_angles[1]), 0),
+                (0, 1, 0, 0),
+                (-np.sin(rotation_angles[1]), 0, np.cos(rotation_angles[1]), 0),
+                (0, 0, 0, 1),
+            )
+        )
+        rotation_z_matrix = np.array(
+            (
+                (np.cos(rotation_angles[2]), -np.sin(rotation_angles[2]), 0, 0),
+                (np.sin(rotation_angles[2]), np.cos(rotation_angles[2]), 0, 0),
+                (0, 0, 1, 0),
+                (0, 0, 0, 1),
+            )
+        )
+
+        # combine
+        output_affine: np.ndarray = (
+            scale_matrix
+            @ translation_matrix
+            @ np.linalg.inv(rotation_origin_translation_matrix)
+            @ rotation_z_matrix
+            @ rotation_y_matrix
+            @ rotation_x_matrix
+            @ rotation_origin_translation_matrix
+            @ input_affine
+        )
+
+        self.outputs[AffineMatrixFilter.KEY_AFFINE] = output_affine
+
+    def _validate_inputs(self):
+        """ Checks that the inputs meet their validation criteria
+        
+        `'rotation'` (optional) must be a Tuple of floats of length 3, each value -180 to 180
+        inclusive,default (optional) = (0.0, 0.0, 0.0)
+        `'rotation_origin'` (optional) must be a Tuple of floats of length 3,
+        default = (0.0, 0.0, 0.0)
+        `'translation'` (optional) must be a Tuple of floats of length 3, default = (0.0, 0.0, 0.0)
+        `'scale'` (optional) must be a Tuple of floats of length 3, default = (1.0, 1.0, 1.0)
+        `'affine'` (optional) must be a numpy.ndarray of shape (4,4), default = numpy.eye(4)
+        """
+
+        input_validator = ParameterValidator(
+            parameters={
+                AffineMatrixFilter.KEY_ROTATION: Parameter(
+                    validators=[
+                        isinstance_validator(tuple),
+                        for_each_validator(isinstance_validator(float)),
+                        for_each_validator(range_inclusive_validator(-180, 180)),
+                    ],
+                    optional=True,
+                    default_value=(0.0, 0.0, 0.0),
+                ),
+                AffineMatrixFilter.KEY_ROTATION_ORIGIN: Parameter(
+                    validators=[
+                        isinstance_validator(tuple),
+                        for_each_validator(isinstance_validator(float)),
+                    ],
+                    optional=True,
+                    default_value=(0.0, 0.0, 0.0),
+                ),
+                AffineMatrixFilter.KEY_TRANSLATION: Parameter(
+                    validators=[
+                        isinstance_validator(tuple),
+                        for_each_validator(isinstance_validator(float)),
+                    ],
+                    optional=True,
+                    default_value=(0.0, 0.0, 0.0),
+                ),
+                AffineMatrixFilter.KEY_SCALE: Parameter(
+                    validators=[
+                        isinstance_validator(tuple),
+                        for_each_validator(isinstance_validator(float)),
+                    ],
+                    optional=True,
+                    default_value=(1.0, 1.0, 1.0),
+                ),
+                AffineMatrixFilter.KEY_AFFINE: Parameter(
+                    validators=[isinstance_validator(np.ndarray)],
+                    optional=True,
+                    default_value=np.eye(4),
+                ),
+            }
+        )
+
+        # validate, returning a dictionary which also includes default parameters
+        new_params = input_validator.validate(
+            self.inputs, error_type=FilterInputValidationError
+        )
+
+        # Further validation that can't be handled by the parameter validator
+        # Check that AffineMatrixFilter.KEY_AFFINE is of size 4x4
+        if new_params[AffineMatrixFilter.KEY_AFFINE].shape != (4, 4):
+            raise FilterInputValidationError
+
+        # Check that the tuple AffineMatrixFilter.KEY_ROTATION's length is 3
+        if len(new_params[AffineMatrixFilter.KEY_ROTATION]) != 3:
+            raise FilterInputValidationError
+
+        # Check that the tuple AffineMatrixFilter.KEY_ROTATION_ORIGIN's length is 3
+        if len(new_params[AffineMatrixFilter.KEY_ROTATION_ORIGIN]) != 3:
+            raise FilterInputValidationError
+
+        # Check that the tuple AffineMatrixFilter.KEY_TRANSLATION's length is 3
+        if len(new_params[AffineMatrixFilter.KEY_TRANSLATION]) != 3:
+            raise FilterInputValidationError
+
+        # Check that the tuple AffineMatrixFilter.KEY_SCALE's length is 3
+        if len(new_params[AffineMatrixFilter.KEY_SCALE]) != 3:
+            raise FilterInputValidationError
+
+        # merge the updated parameters from the output with the input parameters
+        self.inputs = {**self._i, **new_params}
---
+++ src/asldro/filters/test_affine_matrix_filter.py
@@ -0,0 +1,214 @@
+""" Affine Matrix Filter tests """
+
+from copy import deepcopy
+import pytest
+import numpy as np
+import numpy.testing
+from asldro.filters.basefilter import BaseFilter, FilterInputValidationError
+from asldro.filters.affine_matrix_filter import AffineMatrixFilter
+
+# input validation dictionary, [0] in each tuple passes, after that should fail validation
+INPUT_VALIDATION_DICTIONARY = {
+    "rotation": (
+        (180.0, -180.0, 0.0),
+        1.0,
+        (181, 300, 234.2),
+        (0.0, 0.0, 0.0, 0.0),
+        "str",
+    ),
+    "rotation_origin": ((1.0, 2.0, 3.0), 1.0, (int(1), int(2), int(3)), "str"),
+    "translation": (
+        (1.0, 2.0, 3.0),
+        1.0,
+        (int(1), int(2), int(3)),
+        (0.0, 0.0, 0.0, 0.0),
+        "str",
+    ),
+    "scale": (
+        (1.0, 2.0, 3.0),
+        1.0,
+        (int(1), int(2), int(3)),
+        (0.0, 0.0, 0.0, 0.0),
+        "str",
+    ),
+    "affine": (np.eye(4), np.eye(5), 0, (0.0, 0.0, 0.0, 0.0), "str"),
+}
+
+# mock data: rotation, rotation_offset, translation, scale, expected
+MOCK_DATA = (
+    (
+        (0.0, 90.0, 0.0),  # rotate 90 degrees about y
+        (0.0, 0.0, 0.0),
+        (0.0, 0.0, 0.0),
+        (1.0, 1.0, 1.0),
+        (1.0, 0.0, 0.0, 1.0),
+        (0.0, 0.0, -1.0, 1.0),
+    ),
+    (
+        (0.0, 180.0, 0.0),  # rotate 180 degrees about y
+        (0.0, 0.0, 0.0),
+        (0.0, 0.0, 0.0),
+        (1.0, 1.0, 1.0),
+        (1.0, 0.0, 0.0, 1.0),
+        (-1.0, 0.0, 0.0, 1.0),
+    ),
+    (
+        (0.0, 0.0, 90.0),  # rotate 90 degrees about z
+        (0.0, 0.0, 0.0),
+        (0.0, 0.0, 0.0),
+        (1.0, 1.0, 1.0),
+        (1.0, 0.0, 0.0, 1.0),
+        (0.0, 1.0, 0.0, 1.0),
+    ),
+    (
+        (0.0, 0.0, 90.0),  # rotate 90 degrees about z
+        (-1.0, 0.0, 0.0),  # rotation offset at (-1,0,0)
+        (0.0, 0.0, 0.0),
+        (1.0, 1.0, 1.0),
+        (1.0, 0.0, 0.0, 1.0),
+        (-1.0, 2.0, 0.0, 1.0),
+    ),
+    (
+        (0.0, 0.0, 0.0),
+        (0.0, 0.0, 0.0),
+        (1.0, 1.0, 1.0),  # translate by (1,0,0)
+        (1.0, 1.0, 1.0),
+        (1.0, 0.0, 0.0, 1.0),
+        (2.0, 1.0, 1.0, 1.0),
+    ),
+    (
+        (0.0, 0.0, 0.0),
+        (0.0, 0.0, 0.0),
+        (0.0, 0.0, 0.0),
+        (10.0, 5.0, 2.5),  # scale by (10,5,2.5)
+        (1.0, 1.0, 1.0, 1.0),
+        (10.0, 5.0, 2.5, 1.0),
+    ),
+    (
+        (0.0, 0.0, 90.0),  # rotate about z by 90 degrees
+        (5.0, 0.0, 0.0),  # rotation origin at (5, 0, 0)
+        (0.0, 5.0, 0.0),  # translate by (0, 5, 0)
+        (2.0, 2.0, 2.0),  # scale isotropically factor 2
+        (1.0, 1.0, 1.0, 1.0),
+        (8.0, 2.0, 2.0, 1.0),
+    ),
+)
+
+
+def add_multiple_inputs_to_filter(input_filter: BaseFilter, input_data: dict):
+    """ Adds the data held within the input_data dictionary to the filter's inputs """
+    for key in input_data:
+        input_filter.add_input(key, input_data[key])
+
+    return input_filter
+
+
+@pytest.mark.parametrize("validation_data", [INPUT_VALIDATION_DICTIONARY])
+def test_affine_matrix_filter_validate_inputs(validation_data: dict):
+
+    # Check with all data that should pass
+    affine_filter = AffineMatrixFilter()
+    test_data = deepcopy(validation_data)
+
+    for data_key in test_data:
+        affine_filter.add_input(data_key, test_data[data_key][0])
+
+    # should run with no issues
+    affine_filter.run()
+
+    for inputs_key in validation_data:
+        affine_filter = AffineMatrixFilter()
+        test_data = deepcopy(validation_data)
+
+        # remove key
+        test_data.pop(inputs_key)
+        for data_key in test_data:
+            affine_filter.add_input(data_key, test_data[data_key][0])
+        # Key not defined - should still pass as all keys are optional
+        affine_filter.run()
+
+        # Try data that should fail
+        for test_value in validation_data[inputs_key][1:]:
+            affine_filter = AffineMatrixFilter()
+            for data_key in test_data:
+                affine_filter.add_input(data_key, test_data[data_key][0])
+            affine_filter.add_input(inputs_key, test_value)
+            with pytest.raises(FilterInputValidationError):
+                affine_filter.run()
+
+
+def test_affine_matrix_filter_default_data():
+
+    # filter with no inputs, should produce a 4x4 identity matrix
+    affine_filter = AffineMatrixFilter()
+    # add an unused input to set needs_run to True within the filter
+    affine_filter.add_input("test", None)
+    affine_filter.run()
+    numpy.testing.assert_array_equal(affine_filter.outputs["affine"], np.eye(4))
+
+    affine_filter = AffineMatrixFilter()
+    params = {
+        "rotation": (0.0, 0.0, 0.0),
+        "rotation_origin": (0.0, 0.0, 0.0),
+        "translation": (0.0, 0.0, 0.0),
+        "scale": (1.0, 1.0, 1.0),
+        "affine": np.eye(4),
+    }
+    for key in params:
+        affine_filter.add_input(key, params[key])
+    affine_filter.run()
+    numpy.testing.assert_array_equal(affine_filter.outputs["affine"], np.eye(4))
+
+    # test that with default parameters, the input affine is preserved
+    affine_filter = AffineMatrixFilter()
+    params = {
+        "rotation": (0.0, 0.0, 0.0),
+        "rotation_origin": (0.0, 0.0, 0.0),
+        "translation": (0.0, 0.0, 0.0),
+        "scale": (1.0, 1.0, 1.0),
+        "affine": np.array(
+            ((1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (0, 0, 0, 1),)
+        ),
+    }
+    for key in params:
+        affine_filter.add_input(key, params[key])
+    affine_filter.run()
+    numpy.testing.assert_array_equal(affine_filter.outputs["affine"], params["affine"])
+
+
+@pytest.mark.parametrize(
+    "rotation, rotation_origin, translation, scale, vector, expected", MOCK_DATA
+)
+def test_affine_matrix_filter_mock_data(
+    rotation: float,
+    rotation_origin: float,
+    translation: float,
+    scale: float,
+    vector: float,
+    expected: float,
+):
+    """Tests the AffineMatrixFilter by computing the affine matrix, transforming
+    a supplied vector and comparing with a supplied expected vector.
+
+    :param rotation: Tuple of floats describing rotation angles about x, y, z
+    :type rotation: float
+    :param rotation_offset: Tuple of floats describing the coordinates of the rotation origin
+    :type rotation_offset: float
+    :param translation: Tuple of floats describing a translation vector
+    :type translation: float
+    :param scale: Tuple of floats describing scaling along x, y, z
+    :type scale: float
+    :param expected: Tuple of floats describing the expected value
+    :type expected: float
+    """
+
+    affine_filter = AffineMatrixFilter()
+    affine_filter.add_input("rotation", rotation)
+    affine_filter.add_input("rotation_origin", rotation_origin)
+    affine_filter.add_input("translation", translation)
+    affine_filter.add_input("scale", scale)
+    affine_filter.run()
+
+    numpy.testing.assert_array_almost_equal(
+        affine_filter.outputs["affine"] @ vector, expected
+    )
